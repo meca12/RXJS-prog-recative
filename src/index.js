d09fef78b906e0ee78c11824453f2888241defa2
Martha  avila <itit.martha.avil@gmail.com>
2024-08-17 00:24:51 -0600

    Observable practica

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log("HOla"); 
+//construyendo observable
+import { Observable, Subscriber } from "rxjs";
+//Para identificar las variables  que son observables se define  con un $
+//se genera una  instancia de  un  observable 
+const onbservableAlfa$ = new Observable(
+    susbcriber => {
+        //aqui hice  un susbcriber  que genera  una serie de números
+        susbcriber.next(1);
+        susbcriber.next(2);
+        susbcriber.next(3);
+        //complete finaliza el envio de valores
+        susbcriber.complete();
+        susbcriber.next("Hola");
+     
+        susbcriber.next({
+            name: "Martha"
+        }); 
+        susbcriber.next (["Naranja", "Manazanas", "Pepinos"])
+  
+    }); 
+
+/* aqui voy a  crear  un observador 
+que revisara los valores  de mi anterior  observador 
+ */
+
+const observador = {
+    next: (value) => {
+        console.log("Valores recibidos :",value)
+     }, 
+    complete: (complete) => {
+        console.log("Observable completado",complete)
+  
+    },
+    error: (error) => {
+        console.log('Error recibido'); 
+        console.warn(error); 
+    }
+}
+
+/*aqui  voy a crear  un susbscripción para
+ el observable  emita  valores al observador 
+*/
+
+onbservableAlfa$.subscribe(observador); 
