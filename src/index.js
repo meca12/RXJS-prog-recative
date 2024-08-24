fd3aa499a9bb42deb586edc4a55570f7b9a2778b
Martha  avila <itit.martha.avil@gmail.com>
2024-08-23 20:28:46 -0600

    subject

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //construyendo observable
-import { Observable, fromEvent } from "rxjs";
+import { Observable, Subject, fromEvent } from "rxjs";
 //Para identificar las variables  que son observables se define  con un $
 //se genera una  instancia de  un  observable 
 const onbservableAlfa$ = new Observable(
@@ -55,4 +55,31 @@ const observableMOuse = {
         console.log('Mouse','x',event.x, 'y',event.y);  
     }
 }
-mouseMove$.subscribe(observableMOuse)
+mouseMove$.subscribe(observableMOuse);
+
+//--------Subject
+
+const numbers$ = new Observable(subscribe => {
+    subscribe.next(Math.random(Math.random() * 100));
+})
+
+const numberRandom$ = new Subject(); 
+
+const observador1 = {
+    next: (number) => {
+      console.log(number)   
+    }
+}
+
+const observador2 =
+
+{
+    next: (number) => {
+        console.log(number)
+    }
+}
+
+numberRandom$.susbcriber(observador1); 
+numberRandom$.susbcriber(observador2);
+numbers$.susbcriber(numberRandom$); 
+
